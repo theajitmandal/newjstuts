ac912d0f0869582546f635a7727deac953702c2e
ajitdhanukmandal <theajitmandal@gmail.com>
2023-12-04 20:27:50 +0545

    this is next commit

---
+++ newjstutcac/index.js
@@ -0,0 +1,7 @@
+//v#3
+const accountId = 1245;
+let accountName = 'Ajit';
+var accountType = 'Saving';
+
+console.table([accountId, accountName, accountType]);
+console.log(accountType);
---
+++ newjstutcwh/index.js
@@ -0,0 +1,75 @@
+//v#2
+// let a = 34;
+// console.log(a);
+// a = "Ajit";
+// console.log(a);
+
+//v#3
+// // var a = 45;
+// var b ="Ajit";
+// var c = null;
+// var d = undefined;
+
+// {
+//     var b = "Ajit";
+//     console.log(b);
+// }
+
+// console.log(b);
+
+// let a;
+// a = 10;
+// console.log(a);
+
+// var b = 10;
+// var b = 15;
+// console.log(b);
+
+// error
+// let a = 50;
+// var a = 9;
+// console.log(a);
+
+//error
+// var a = 5;
+// let a = 25;
+// console.log(a);
+
+//no error
+// var a = 56;
+// var a = 35;
+// console.log(a);
+
+//v#4
+//nn bb ss u
+
+let a = null;
+let b = 34;
+
+let c = true;
+let d = BigInt("123")
+
+let e = 'Ajit';
+let f = Symbol("I am a symbol");
+
+let g = undefined;
+
+console.log(a, b, c, d, e, f, g);
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+console.log(typeof(g));
+
+//objects
+const item = {
+    itemName: 'Biscuit',
+    itemPrice: 200,
+    itemStock: 20
+}
+
+console.log(item.itemName);
+console.log(item.itemStock);
